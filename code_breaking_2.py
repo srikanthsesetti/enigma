268d55e43cec91e325f6064cddb2e6ea20230535
srikanthsesetti <srikanthsesetti@gmail.com>
2020-10-29 16:37:36 +0000

    completed breaking code 2

---
+++ code_breaking_2.py
@@ -0,0 +1,20 @@
+from run_enigma import *
+import itertools
+import string
+
+# Run this python file to see the decoded message
+
+plugboard_pairs = "VH PT ZG BJ EY FS"
+reflector = "B"
+rotors = "Beta I III"
+ring_settings = "23 02 10"
+message = "CMFSUPKNCBMUYEQVVDYKLRQZTPUFHSWWAKTUGXMPAMYAFITXIJKMH"
+crib = "UNIVERSITY"
+
+for i in itertools.permutations(string.ascii_lowercase, 3):
+    combination = ' '.join(i)
+    decode_message = run_enigma(plugboard_pairs, rotors, reflector, combination, ring_settings, message)
+    if crib in decode_message:
+        print(f'Missing Ring Positions: {combination.upper()}')
+        print(f'Decoded message: {decode_message}')
+        break
