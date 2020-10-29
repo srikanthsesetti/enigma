9273058f9a53854ac70631ebdface9ce204135f5
srikanthsesetti <srikanthsesetti@gmail.com>
2020-10-29 16:36:38 +0000

    completed breaking code 1

---
+++ code_breaking_1.py
@@ -0,0 +1,17 @@
+from run_enigma import *
+
+# Run this python file to see the details of the reflector used and the decoded message
+plugboard_pairs = "KI XN FL"
+reflectors = "ABC"
+crib = "SECRETS"
+rotors = "Beta Gamma V"
+ring_settings = "04 02 14"
+starting_positions = "M J M"
+message = "DMEXBMKYCVPNQBEDHXVPZGKMTFFBJRPJTLHLCHOTKOYXGGHZ"
+
+for reflector in reflectors:
+    decoded_message = run_enigma(plugboard_pairs, rotors, reflector, starting_positions, ring_settings, message)
+    if crib in decoded_message:
+        print(f'Reflector: {reflector}')
+        print(f'Decoded message:  {decoded_message}')
+        break
